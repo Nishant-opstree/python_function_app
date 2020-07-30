5df506aa78044375fca2beba2b61d201603406c7
trickste <nishantprakashsaini285@gmail.com>
2020-07-30 05:51:05 +0530

    upload

--- func_1_http/httptrigger_1/__init__.py
+++ func_1_http/httptrigger_1/__init__.py
@@ -1,24 +1,18 @@
 import logging
+import json
 
 import azure.functions as func
 
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    products = [
+        {
+            "name": "Azure DevOps",
+            "price": 4.99
+        },
+        {
+            "name": "Azure Functions",
+            "price": 0.01
+        }
+    ]
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(json.dumps(products))
